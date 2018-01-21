df99bc13bdc4eaf8d96712c0ef4a5a020dc3b3ff
seanjohnson08 <seanjohnson08@gmail.com>
2018-01-21 12:58:06 -0500

    Fix test failure

--- src/helpers/Currency.test.js
+++ src/helpers/Currency.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Currency, {formatCurrencyNumber}  from './Currency';
+import Currency, { formatCurrencyNumber } from './Currency';
 
 it('formats correctly', () => {
   expect(Currency(10000)).toBe('$100.00');
   expect(Currency(999999)).toBe('$9,999.99');
   expect(formatCurrencyNumber(10000)).toBe('100.00');
-  expect(formatCurrencyNumber(999999)).toBe('9,999.99');
+  expect(formatCurrencyNumber(999999)).toBe('9999.99');
 });
