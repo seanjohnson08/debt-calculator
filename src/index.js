925589bc9f16a5dfed10b04cd283b237d062efed
seanjohnson08 <seanjohnson08@gmail.com>
2018-01-29 22:15:49 -0500

    Upgrade to Bootstrap@4 CDN

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom';
 // App Template
 import App from './App.jsx';
 
-// CSS:
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
 import './styles/App.css';
 
 import registerServiceWorker from './registerServiceWorker';
