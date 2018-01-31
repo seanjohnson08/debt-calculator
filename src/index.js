5f1370ebdd21cc97cb9f2e57587eba5f426f6f28
seanjohnson08 <seanjohnson08@gmail.com>
2018-01-30 19:46:14 -0500

    Fix zIndex issue with modals

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import registerServiceWorker from './registerServiceWorker';
 
 // App Template
 import App from './App.jsx';
 
+// CSS
 import './styles/App.css';
 
-import registerServiceWorker from './registerServiceWorker';
+// Configure default modal styles
+Modal.defaultStyles.overlay.zIndex = 1050; // taken from https://v4-alpha.getbootstrap.com/layout/overview/
 
 const appElement = document.getElementById('root');
 
